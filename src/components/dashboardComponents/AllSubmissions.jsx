dbe3fff72f7f87f1de44fbb3106f5aab62b1918a
aditya-kumar-25 <aditya.pca.25@gmail.com>
2023-11-03 00:00:36 +0530

    max

--- src/components/dashboardComponents/AllSubmissions.jsx
+++ src/components/dashboardComponents/AllSubmissions.jsx
@@ -21,7 +21,7 @@ function AllSubmissions({ user }) {
           }/${user}`
         );
         setData(res.data?.submissions);
-        setTotalPages(max(1 , Math.ceil(res.data.submissions.length / 5)));
+        setTotalPages(Math.max(1 , Math.ceil(res.data.submissions.length / 5)));
         setRange([0, Math.min(4, res.data.submissions.length - 1)]);
       } catch (err) {
         console.log(err);
